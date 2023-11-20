3e2a14c563597065c76ebec6420560d0ae199a4e
vngson <Soncuhkb1111@gmail.com>
2023-11-20 10:46:46 +0700

    "optimal"

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,162 +3,151 @@ confetti.config = {
     angle: 90,
     spread: width,
     startVelocity: 50,
-    decay: 0.9,
-    ticks: 60,
+    ticks: 20,
+    decay: 0.8,
     zIndex: 9999,
 };
 
-const confettiParticles = []; // Mảng chứa các hạt confetti
+// Create a new confetti effect and launch it for a limited duration
+const launchConfetti = () => {
+  const confettiInterval = setInterval(() => {
+      confetti({
+          particleCount: Math.floor(Math.random() * 30 + 20),
+          origin: {
+              x: Math.random(),
+              y: Math.random() - 0.2
+          },
+          colors: ["#FFD700", "#FFDF00", "#D4AF37", "#F0E68C", "CCAC00", "#FF0000", "#00BFFF", "#50C7C7", "#00FF00"]
+      });
+  }, 100);
 
-// Hàm để tạo một hạt confetti và thêm vào mảng
-const createConfetti = () => {
-    const confettiInstance = confetti({
-        particleCount: Math.floor(Math.random() * 150 + 50),
-        origin: {
-            x: Math.random(),
-            y: Math.random() - 0.2
-        },
-        colors: ["#FFD700", "#FFDF00", "#D4AF37", "#F0E68C","CCAC00","#FF0000", "#00BFFF", "#50C7C7", "#00FF00"],
-    });
-
-    confettiParticles.push(confettiInstance);
+  // Clear the interval after 2 seconds (200 milliseconds)
+  setTimeout(() => {
+      clearInterval(confettiInterval);
+  }, 200);
 };
 
-// Hàm để cập nhật và vẽ hiệu ứng confetti
-const updateConfetti = () => {
-    confettiParticles.forEach((particle, index) => {
-        particle.ticks--;
-
-        if (particle.ticks <= 0) {
-            // Xóa hạt confetti khi hết thời gian
-            confettiParticles.splice(index, 1);
-            particle.reset();
-        }
+const stopConfetti = () => { // Clear the confetti interval
+    document.querySelectorAll('.confetti').forEach((el) => {
+        el.remove();
     });
+};
 
-    if (Math.random() > 0.95) {
-        // Tạo một hạt confetti mới với xác suất 5%
-        createConfetti();
+function numberToEnglish(num) {
+    if (num < 1 || isNaN(num)) {
+        return 'Invalid input';
     }
 
-    requestAnimationFrame(updateConfetti);
-};
+    const lastDigit = num % 10;
+    const secondToLastDigit = Math.floor((num % 100) / 10);
 
-// Khởi tạo hiệu ứng confetti
-const startConfetti = () => {
-    createConfetti();
-    updateConfetti();
-};
+    let suffix = 'th';
 
-const stopConfetti = () => {
-  confettiParticles.forEach((particle) => {
-      particle.reset();
-  });
-  confettiParticles.length = 0; // Xóa mảng hạt confetti
-};
+    if (secondToLastDigit !== 1) {
+        switch (lastDigit) {
+            case 1:
+                suffix = 'st';
+                break;
+            case 2:
+                suffix = 'nd';
+                break;
+            case 3:
+                suffix = 'rd';
+                break;
+        }
+    }
 
-function toEnglish(num) {
-  const specialCases = { 1: '1st', 2: '2nd', 3: '3rd' };
-  const suffixes = ['th', 'st', 'nd', 'rd', 'th'];
- 
-  // For special cases, return the corresponding English representation
-  if (specialCases.hasOwnProperty(num)) {
-     return specialCases[num];
-  }
- 
-  // Convert the number to words using toLocaleString
-  let words = num.toLocaleString('en-US', { style: 'ordinal' });
- 
-  // Get the last character of the word representation, which determines the suffix
-  let lastChar = words.charAt(words.length - 1);
-  let suffixIndex = Number(lastChar);
- 
-  // Replace the suffix of the word representation with the correct English ordinal suffix
-  words = words.slice(0, -1) + suffixes[suffixIndex];
- 
-  return words;
- }
+    return num + suffix;
+}
 
 // Khai báo biến lưu trữ ngày sinh nhật mong muốn
-var birthday = new Date("2023-11-17T22:42:00");
+let birthday = new Date("2023-11-20T09:57:00");
+// Khai báo biến lưu trữ năm sinh mong muốn
+var birthdayYear = 2002;
 // Khai báo biến lưu trữ phần tử html hiển thị thời gian còn lại
 var countdown = document.querySelector(".countdown");
+// Khai báo biến lưu trữ phần tử main
 var main = document.querySelector(".main");
-
-function restartCountdown() {
-  stopConfetti();
-  birthday.setFullYear(birthday.getFullYear() + 1);
-
-    // Restart the countdown
-  interval = setInterval(updateCountdown, 500);
-}
-// Khai báo hàm để cập nhật thời gian còn lại
-function updateCountdown() {
-  countdown.style.display = "flex";
-      // Hiển thị thẻ B bằng cách đặt display là block
+// Khai báo biến tính khoảng cách từ now đến birthday
+let distance;
+// Khai báo hàm để chạy chương trình
+function mainFunction() {
+    // Thay thế 'your-audio-element-id' bằng ID của thẻ audio của bạn
+    const audioElement = document.querySelector('.main__birthday-audio'); 
+    countdown.style.display = "flex";
     main.style.display = "none";
-  // Lấy thời gian hiện tại
-  var now = new Date();
-
-  let age = birthday.getFullYear() - 2002;
-  let ageOrdinal =  toEnglish(age);
-  var main__wish = document.querySelector(".main__wish");
-  main__wish.innerHTML = `Happy special day, my ${ageOrdinal} birthday`
-  // Tính khoảng cách giữa thời gian hiện tại và ngày sinh nhật
-  var distance = birthday - now;
-
-  // Nếu khoảng cách nhỏ hơn hoặc bằng 0, tức là đã đến ngày sinh nhật
-  if (distance <= 0) {
-    // Dừng hàm setInterval
-    clearInterval(interval);
-    // Chạy chương trình chính do bạn code
-    countdown.style.display = "none";
-    main.style.display = "flex";
-    startConfetti();
-
-  // Chờ đợi 1 ngày trước khi chạy đoạn mã tiếp theo
-    // if(distance === -5000)
-    // {
-    //     // Dừng hiệu ứng confetti
-    //     stopConfetti();
-    //     // Cập nhật ngày sinh nhật cho năm tiếp theo
-    //     birthday.setFullYear(birthday.getFullYear() + 1);
-
-    //     // Restart the countdown
-    //     interval = setInterval(updateCountdown, 500);
-    // } // 24 giờ
-  }else {
+    // Khai báo biến lưu ngày hiện tại
+    var now = new Date();
+    // Gán giá trị khoảng cách từ now đến birthday
+    distance = birthday - now;
+    //Khai báo biến giá trị tuổi
+    let age = Number(birthday.getFullYear() - birthdayYear);
+    // Chuyển đổi sang dạng tiếng anh
+    let ageOrdinal =  numberToEnglish(age);
+    // Khai báo biến lưu trữ phần tử main__wish
+    var main__wish = document.querySelector(".main__wish");
+    // Gán giá trị innerHTML phần tử main__wish
+    main__wish.innerHTML = `Happy special day, my ${ageOrdinal} birthday`
+    // Nếu khoảng cách nhỏ hơn hoặc bằng 0, tức là đã đến ngày sinh nhật 
+    if (distance < 0 && distance >= -86400000) {
+        // Chạy chương trình chính do bạn code
+        countdown.style.display = "none";
+        main.style.display = "flex";
+        // Sự kiện mousedown để giả mạo sự tương tác người dùng
+        document.addEventListener('mousedown', function() {
+            // Phát âm thanh
+            audioElement.play();
+            // Gỡ bỏ sự kiện mousedown để tránh lặp lại
+            document.removeEventListener('mousedown', arguments.callee);
+        });
+        launchConfetti();
+    }
+    // Chờ đợi 1 ngày sau khi chúc sinh nhật thì khởi động lại việc đếm ngược đến sinh nhật năm tiếp theo
+    else if (distance < -86400000) // 24 giờ 
+    {
+        countdown.style.display = "flex";
+        main.style.display = "none";
+        audioElement.pause();
+        audioElement.currentTime = 0;
+        // Dừng hiệu ứng confetti
+        stopConfetti();
+        birthday.setFullYear(birthday.getFullYear() + 1);
+    }
     // Nếu khoảng cách lớn hơn 0, tức là chưa đến ngày sinh nhật
-
-    // Tính số ngày, giờ, phút và giây còn lại
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var day_hundreds = document.getElementById("day_hundreds");
-    day_hundreds.innerHTML = Math.floor(days/100);
-    var day_dozens = document.getElementById("day_dozens");
-    day_dozens.innerHTML = Math.floor((days%100)/10);
-    var day_unit = document.getElementById("day_unit");
-    day_unit.innerHTML = Math.floor((days%100)%10);
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
-    var hour_dozens = document.getElementById("hour_dozens");
-    hour_dozens.innerHTML = Math.floor(hours /10);
-    var hour_unit = document.getElementById("hour_unit");
-    hour_unit.innerHTML = Math.floor(hours %10);
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var minute_dozens = document.getElementById("minute_dozens");
-    minute_dozens.innerHTML = Math.floor(minutes /10);
-    var minute_unit = document.getElementById("minute_unit");
-    minute_unit.innerHTML = Math.floor(minutes %10);
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    var second_dozens = document.getElementById("second_dozens");
-    second_dozens.innerHTML = Math.floor(seconds /10);
-    var second_unit = document.getElementById("second_unit");
-    second_unit.innerHTML = Math.floor(seconds %10);
-  }
+    else
+    {
+        // Tính số ngày, giờ, phút và giây còn lại
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var day_hundreds = document.getElementById("day_hundreds");
+        day_hundreds.innerHTML = Math.floor(days / 100);
+        var day_dozens = document.getElementById("day_dozens");
+        day_dozens.innerHTML = Math.floor((days % 100) / 10);
+        var day_unit = document.getElementById("day_unit");
+        day_unit.innerHTML = Math.floor((days % 100) % 10);
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var hour_dozens = document.getElementById("hour_dozens");
+        hour_dozens.innerHTML = Math.floor(hours / 10);
+        var hour_unit = document.getElementById("hour_unit");
+        hour_unit.innerHTML = Math.floor(hours % 10);
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var minute_dozens = document.getElementById("minute_dozens");
+        minute_dozens.innerHTML = Math.floor(minutes / 10);
+        var minute_unit = document.getElementById("minute_unit");
+        minute_unit.innerHTML = Math.floor(minutes % 10);
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var second_dozens = document.getElementById("second_dozens");
+        second_dozens.innerHTML = Math.floor(seconds / 10);
+        var second_unit = document.getElementById("second_unit");
+        second_unit.innerHTML = Math.floor(seconds % 10);
+    }
+    setTimeout(mainFunction, 200);
 }
 
 // Khai báo biến lưu trữ hàm setInterval
-let interval = setInterval(updateCountdown, 500);
+mainFunction();
 
+
+//Hàm chúc mừng ngày 20/10
 
 // var count = 0;
 
